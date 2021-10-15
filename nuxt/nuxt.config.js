b9f5f7f22478207b07a72650d012194d6b62f782
Stuart Clark <stu@rtclark.net>
2021-10-15 10:14:53 +0000

    chore: setup druxt

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -5,7 +5,10 @@ export default {
   ],
 
   druxt: {
-    baseUrl: `https://8080-${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
+    baseUrl: `https://8080-${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`,
+    endpoint: '/api/v1',
+    entity: { components: { fields: false }},
+    site: { layout: false },
   },
 
   telemetry: true
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <DruxtEntity type="node--page" uuid="5a0291e0-f649-40fe-97c7-8cb4df722db9" />
+</template>
