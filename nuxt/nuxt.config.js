5fc99426a1d96607d1a6e014fb8f7feb17208682
Stuart Clark <stu@rtclark.net>
2022-01-06 23:34:38 +0000

    feat: add Vuetify

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,11 +1,19 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   buildModules: [
     '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify',
     'druxt-site',
   ],
 
   druxt: {
     baseUrl: `https://8080-${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
+  },
+
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: { dark: false }
   },
 
   telemetry: true
