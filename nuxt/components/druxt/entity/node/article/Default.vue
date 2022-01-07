94eed275b734c4d49a20fb61c6d4a09ed8ed14fa
Stuart Clark <stu@rtclark.net>
2022-01-07 01:56:57 +0000

    feat: add related articles view

--- nuxt/components/druxt/entity/node/article/Default.vue
+++ nuxt/components/druxt/entity/node/article/Default.vue
@@ -1,10 +1,46 @@
 <template>
   <VRow>
+    <VCol :cols="12">
+      <h1 v-text="entity.attributes.title" />
+    </VCol>
     <VCol :cols="8">
-      <slot />
+      <VChip
+        v-for="(tag, key) of tags"
+        :key="key"
+        v-text="tag.attributes.name"
+      />
+
+      <slot name="body" />
     </VCol>
     <VCol :cols="4">
-      @TODO - Related article view.
+      <DruxtView
+        :arguments="viewArgs"
+        view-id="related_articles"
+      />
     </VCol>
   </VRow>
 </template>
+
+<script>
+import { DruxtEntityMixin } from 'druxt-entity'
+
+export default {
+  mixins: [DruxtEntityMixin],
+  computed: {
+    tags: ({ entity }) => entity.included.filter((o) => o.type === 'taxonomy_term--tags'),
+    viewArgs: ({ entity, tags }) => [
+      entity.attributes.drupal_internal__nid,
+      tags.map((o) => o.attributes.drupal_internal__tid).join('+')
+    ]
+  },
+  druxt: {
+    query: {
+      fields: [
+        ['body', 'drupal_internal__nid', 'title'],
+        ['taxonomy_term--tags', ['drupal_internal__tid', 'name']]
+      ],
+      include: ['field_tags'],
+    },
+  },
+}
+</script>
--- nuxt/components/druxt/entity/node/article/Teaser.vue
+++ nuxt/components/druxt/entity/node/article/Teaser.vue
@@ -1,8 +1,19 @@
 <template>
   <VCard>
-    <VImg src="https://cdn.vuetifyjs.com/images/cards/cooking.png" />
+    <VImg
+      :src="imgSrc"
+      height="150"
+    />
+
+    <VCardTitle v-text="entity.attributes.title" />
 
     <VCardText>
+      <VChip
+        v-for="(tag, key) of tags"
+        :key="key"
+        v-text="tag.attributes.name"
+      />
+
       <div v-text="summary" />
     </VCardText>
 
@@ -24,7 +35,19 @@ import ellipsize from 'ellipsize'
 export default {
   mixins: [DruxtEntityMixin],
   computed: {
+    imgSrc: ({ entity }) => entity.included.find((o) => o.type === 'file--file').attributes.uri.url,
     summary: ({ fields }) => ellipsize(fields.body.data.summary, fields.body.schema.settings.display.trim_length),
-  }
+    tags: ({ entity }) => entity.included.filter((o) => o.type === 'taxonomy_term--tags'),
+  },
+  druxt: {
+    query: {
+      fields: [
+        ['body', 'drupal_internal__nid', 'field_image', 'title'],
+        ['file--file', ['uri']],
+        ['taxonomy_term--tags', ['name']]
+      ],
+      include: ['field_image', 'field_tags'],
+    },
+  },
 }
 </script>
