d94df5a739631c0172582b0c6d184ed45efce365
Stuart Clark <stu@rtclark.net>
2022-02-08 03:19:00 +0000

    feat: add WIP node preview frontend

---
+++ nuxt/pages/node/preview/_view_mode.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ url }}
+    {{ entity }}
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'preview',
+
+  data: () => ({
+    entity: null,
+  }),
+
+  async fetch() {
+    try {
+      const response = await this.$druxt.axios.get(this.url)
+      this.entity = response.data
+    } catch(err) {
+      console.log('ERR', err)
+    }
+  },
+
+  computed: {
+    url: ({ $route }) => $route.hash.substring(1).replace(':/', '://')
+  }
+}
+</script>
