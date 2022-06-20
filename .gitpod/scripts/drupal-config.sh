19bad90cf2488dd7c3aa4b7464ba315192d1607c
Stuart Clark <stu@rtclark.net>
2022-06-20 00:33:50 +0000

    chore: update node preview setup

--- .gitpod/scripts/drupal-config.sh
+++ .gitpod/scripts/drupal-config.sh
@@ -3,4 +3,4 @@
 DRUPAL_DIR="${GITPOD_REPO_ROOT}/drupal"
 
 echo "frontends: \"GitPod|https://3000-$GITPOD_WORKSPACE_ID.$GITPOD_WORKSPACE_CLUSTER_HOST/node/preview/[view_mode]#[jsonapi_node_preview]\"" > $DRUPAL_DIR/config/druxt_node_preview.settings.yml
-cd "$DRUPAL_DIR" && ddev cim -y
+cd "$DRUPAL_DIR" && ddev drush cim -y
