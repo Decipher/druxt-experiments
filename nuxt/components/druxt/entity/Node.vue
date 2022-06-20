5c6a321b55fdb973ee233f92730657f3379cf019
Stuart Clark <stu@rtclark.net>
2022-06-20 22:56:01 +0000

    chore: add persistedstate

---
+++ nuxt/components/druxt/entity/Node.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <component :is="component.is" v-bind="component.bind">
+    <h2 v-text="entity.attributes.title" />
+    <slot />
+  </component>
+</template>
+
+<script>
+import { DruxtEntityMixin } from 'druxt-entity'
+
+export default {
+  mixins: [DruxtEntityMixin],
+
+  computed: {
+    component: ({ $parent, entity }) => ({
+      is: $parent.mode === 'full' ? 'div' : 'nuxt-link',
+      bind: $parent.mode === 'full' ? undefined : {
+        to: entity.attributes.path.alias
+      }
+    })
+  }
+}
+</script>
--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -4,6 +4,8 @@ export default {
     'druxt-site',
   ],
 
+  plugins: [{ src: '~/plugins/vuex-persistedstate.client.js' }],
+
   druxt: {
     baseUrl: `https://8080-${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
   },
